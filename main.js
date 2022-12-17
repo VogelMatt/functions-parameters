ecf8a0475a65b50054fe251a659257a610d03f4e
ToastyandTasty <mahvogel@gmail.com>
2022-12-17 10:30:24 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const findQuarter = (numToQuarter) => {
+    const newValue = numToQuarter / 4;
+    return newValue 
+
+}
+// Invoke the function and store the return value
+const returnedNum = findQuarter(8)
+
+// Log the return value
+console.log(returnedNum);
